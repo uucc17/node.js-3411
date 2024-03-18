7ed613c2f1a50cbf6b6f8117984407e60af72b90
uucc17 <w2213@e-mirim.hs.kr>
2024-03-18 10:43:57 +0900

    0318 ex 연습

---
+++ node/ex/ex04.js
@@ -0,0 +1,21 @@
+// 일반형 
+// - (매개변수) => {함수코드}
+func_arrow1 = (str) => {
+    return "출력 1 : " + str
+};
+
+// 매개변수가 하나일때는 () 생략 가능
+// - 매개변수 => {함수코드}
+func_arrow2 = str => {
+    return "출력 2 : " + str
+};
+
+// 함수코드가 return문이 하나일경우 괄호 및 return 생략 가능
+// - 매개변수 => 반환값
+func_arrow3 = str => "출력 3 : " + str
+console.log(func_arrow1('fa1'));
+console.log(func_arrow2('fa2'));
+console.log(func_arrow3('fa3'));
+
+/* arrow function 과 function 의 차이점은
+    */

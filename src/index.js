4b7bad547c1f3f7d862425c0cb3d81ccad430748
uucc17 <w2213@e-mirim.hs.kr>
2024-04-15 11:49:12 +0900

    정리

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const middle1 = require("./my_modules/middle1")
 app.use(middle1);
 
 app.get('/', function(req, res){
-    console.log(":", path.join(__dirname,"html")); //ㅇ?
+    console.log("경로:", path.join(__dirname,"html")); //ㅇ?
     res.send('Hello World2');
 });
 
 app.get('/home', function(req, res){
     res.sendFile(__dirname+'/home.html');
-    res.send('홈입니다');
+    res.send('홈입니다.');
 });
 
 app.get('/grape/:name', function(req, res){

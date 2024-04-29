6fbf112fba45323887cb83a385dcc6bc2a53fca6
uucc17 <w2213@e-mirim.hs.kr>
2024-04-29 10:25:57 +0900

    RE미들웨어

--- src/index(1).js
+++ src/index(1).js
@@ -1,14 +1,43 @@
 const express = require('express');
-const cookieParser = require('cookie-pearser')
 const app = express();
+const path = require('path');
 
-app.use(cookieParser());
+// 미들웨어 함수
+// const middle1 = (req, res, next) =>{
+//     console.log("미들웨어 실행");
+//     next();
+// };
 
-app.get('/', (req, res) => {
-    res.cookie("cookie", "chickchock",{
-        maxAge:50000
-    });
-    res.send("<h1>홈입니다.</h1>")
+const middle1 = require("./my_modules/middle1")
+
+app.use(middle1);
+
+app.all("/all", (req, res, next)=>{
+    res.send("all");
+    next();
+});
+
+app.use("/use",(req, res, next)=>{
+    res.send("use");
+    next();
+});
+
+app.get('/', function(req, res, next){
+    console.log("경로:", path.join(__dirname,"html")); //ㅇ?
+    // res.send('Hello World2');
+});
+
+app.get('/home', function(req, res){
+    res.sendFile(__dirname+'/home.html');
+    console.log("두번째 + 입니다,");
+    res.send('홈입니다.');
+});
+
+app.get('/grape/:name', function(req, res){
+    console.log('path', req.path);
+    console.log('params', req.params);
+    console.log('query', req.query);
+    res.send("포도페이지입니다.");
 });
 
 app.listen(3000);
--- src/index.js
+++ src/index.js
@@ -1,43 +1,26 @@
 const express = require('express');
+const cookieParser = require('cookie-pearser')
 const app = express();
-const path = require('path');
 
-// 미들웨어 함수
-// const middle1 = (req, res, next) =>{
-//     console.log("미들웨어 실행");
-//     next();
-// };
+app.use(cookieParser());
 
-const middle1 = require("./my_modules/middle1")
-
-app.use(middle1);
-
-app.all("/all", (req, res, next)=>{
-    res.send("all");
-    next();
+app.get('/', (req, res) => {
+    res.cookie("cookie", "chickchock",{
+        maxAge:50000
+    });
+    res.send("<h1>홈입니다.</h1>")
 });
 
-app.use("/use",(req, res, next)=>{
-    res.send("use");
-    next();
+app.get("/cookie", (req, res) => {
+    const c1 = req.cookies.cookie;
+    res.send("<h1>쿠키페이지입니다</h1>")
 });
 
-app.get('/', function(req, res, next){
-    console.log("경로:", path.join(__dirname,"html")); //ㅇ?
-    // res.send('Hello World2');
-});
+app.get("/clear",(req,res)=>{
+    res.clearCookie("cookie");
+    res.send("<h1>쿠키가 삭제되었습니다.");
+})
 
-app.get('/home', function(req, res){
-    res.sendFile(__dirname+'/home.html');
-    console.log("두번째 + 입니다,");
-    res.send('홈입니다.');
-});
-
-app.get('/grape/:name', function(req, res){
-    console.log('path', req.path);
-    console.log('params', req.params);
-    console.log('query', req.query);
-    res.send("포도페이지입니다.");
-});
+app.get("/login", (req, res))
 
 app.listen(3000);

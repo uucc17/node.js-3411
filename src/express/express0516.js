731e449bee8aa7fbbff918dd2d6d78a84cf163ff
uucc17 <w2213@e-mirim.hs.kr>
2024-05-16 15:25:07 +0900

    수정 추가

--- src/express/express0516.js
+++ src/express/express0516.js
@@ -6,6 +6,15 @@ app.use(session({
     secret:"AnimalBannaCandy",
 }));
 
+app.get("/setUser", (req, res)=>{
+    req.session.user = {name:"kim", age:5}
+    res.redirect("/getUser");
+});
+
+app.get("/getUser", (req, res)=>{
+    res.send(req.session.user);
+});
+
 app.listen(8839, () => {
     console.log("8839이 포트입니다")
 })
